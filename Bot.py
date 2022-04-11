42ab3347b73e53d29ad2d6ce1e354951410d8039
athomsAF <102578896+athomsAF@users.noreply.github.com>
2022-04-11 16:32:02 +0200

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -1,3 +1,15 @@
 import discord
-from discord.ext import comands
+from discord.ext import commands
 import youtube_dl
+
+bot = commands.Bot(command_prefix ="!", description = "Bot du Fab")
+musics ={}
+ytdl=""
+
+@bot.event
+async def on_ready():
+    print("Ready !")
+    channel = bot.get_channel(963007190308892702963007190308892702)
+    await channel.send("Salut je suis allumé!!!")
+
+bot.run("OTYyNzc4OTczODA3NDQ4MDk1.YlMfyg.XnRmFuH4O-iRFHeFcQ5Em26JiIU")
