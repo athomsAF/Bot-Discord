8118bbf69a53473a22cc58ce7112edc98f619dd9
athomsAF <thomas.drddr03@gmail.com>
2022-04-11 18:11:24 +0200

    correction du channel

--- Bot.py
+++ Bot.py
@@ -9,7 +9,7 @@ ytdl=""
 @bot.event
 async def on_ready():
     print("Ready !")
-    channel = bot.get_channel(963007190308892702963007190308892702)
+    channel = bot.get_channel(963007190308892702)
     await channel.send("Salut je suis allumé!!!")
 
 bot.run("OTYyNzc4OTczODA3NDQ4MDk1.YlMfyg.XnRmFuH4O-iRFHeFcQ5Em26JiIU")
