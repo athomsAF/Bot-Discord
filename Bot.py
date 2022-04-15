4d73f7a633da762fd20b0db881c54ed51b7d879c
Yann Vidamment <yannvidamment@MacBook-Air-de-Yann.local>
2022-04-15 16:00:07 +0200

    Ajout kick, status, mem

--- Bot.py
+++ Bot.py
@@ -2,31 +2,40 @@ import discord
 from discord.ext import commands
 import youtube_dl
 
-bot = commands.Bot(command_prefix ="$", description = "Bot du Fab")
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix ="$", description = "On découvre python c nice",intents=intents)
 
 @bot.event
 async def on_ready():
     print("--- Ready ---")
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Netflix"))
     channel = bot.get_channel(963007190308892702)
-    await channel.send("Turned on")
-
+    await channel.send("< ON >")
+    #guil=ctx.message.guild.id
+@bot.command()  
+async def mem(ctx):
+  members = await ctx.guild.fetch_members(limit=150).flatten()
+  for member in members:
+    await ctx.send(f"{member.name} = {member.id}")
+    
 @bot.command(aliases= ['hi'])
 async def Hi(ctx):
     await ctx.send(f"Yoosh {ctx.author.mention} !", )
 
-@bot.command(aliases= ['purge','Purge','prg','Prg','delete','Delete','del','Del','dl','Dl','suppr','clr','Clr','cl','Cl','Clear']) #clear command
+@bot.command(aliases= ['clear','Cl','CL','cl']) #clear command
 @commands.has_permissions(manage_messages=True)
-async def clear(ctx,amount: int = None):
+async def Clear(ctx,amount: int = None):
     if amount == None:
         await ctx.channel.purge(limit=1000000)
     else:
-       await ctx.channel.purge(limit=amount)
+       await ctx.channel.purge(limit=amount+1)
 
 @bot.command()
-async def Parrot(ctx, arg):
-    await ctx.send(arg, tts = True)
+async def Parrot(ctx, *arg):
+    await ctx.send(" ".join(arg), tts = True)
 
-@bot.command(aliases = ['InfoServeur'])
+@bot.command(aliases= ['Infoserveur','Info','info'])
 async def InfoServeur(ctx):
     server = ctx.guild
     NombreChanText = len(server.text_channels)
@@ -35,6 +44,19 @@ async def InfoServeur(ctx):
     NomServeur = server.name
     message = f"Le serveur **{NomServeur}** contient {NombrePersonnes} personnes. \nLe serveur possède {NombreChanText} salons textuels et {NombreChanVoc} salons vocaux."
     await ctx.send(message)
+
+@bot.command()
+async def kick(ctx, user : discord.User, *reason):
+    reason = " ".join(reason)
+    await ctx.guild.kick(user , reason = reason)
+    await ctx.send(f"{user} a été kick.")
+
+@bot.command()
+async def Poll(ctx, question, options):
+ 
+    
+    for i in range(int(options)):
+        await ctx.send(f":{i}:")
 
 
 #   /usr/bin/python3 "/Volumes/Macintosh HD - Données/FICHIERS/ESILV/VsCode/Bot-Discord/Bot.py"
