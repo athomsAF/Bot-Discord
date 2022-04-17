f08f2f7abbb048ea8e7ac6cb44ed62237be47736
Yann Vidamment <yannvidamment@macbook-air-de-yann.home>
2022-04-17 12:01:14 +0200

    commandes musique

--- Bot.py
+++ Bot.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
 import youtube_dl
-
-
+import asyncio
 
 intents = discord.Intents.default()
 intents.members = True
@@ -81,6 +80,72 @@ async def Poll(ctx, question, options, *choice):
         message += choice,"\n"
     
     await ctx.send(message)
+
+#Youtube
+musics = {}
+
+ytdl = youtube_dl.YoutubeDL()
+
+class Video:
+    def __init__(self, link):
+        video = ytdl.extract_info(link, download=False)
+        video_format = video["formats"][0]
+        self.url = video["webpage_url"]
+        self.stream_url = video_format["url"]
+
+def play_song(client, queue, song):
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(song.stream_url, before_options =  "-reconnect 1 - reconnect_streamed 1 - reconnect_delay_max 5"))
+    
+    async def next (_):
+        if len(queue) > 0:
+            new_song = queue[0]
+            del queue[0]
+            play_song(client, queue, new_song)
+        else:
+            asyncio.run_coroutine_threadsafe(client.diconnect(), bot.loop)
+
+    client.play(source, after = next)
+
+
+@bot.command()
+async def skip(ctx):
+    client = ctx.guild.voice_client
+    client.stop()
+
+@bot.command()
+async def leave(ctx):
+    client = ctx.guild.voice_client
+    await client.disconnect()
+    musics[ctx.guild]= []
+
+@bot.command()
+async def resume(ctx):
+    client = ctx.guild.voice_client
+    if client.is_paused():
+        client.resume()
+
+@bot.command()
+async def pause(ctx):
+    client = ctx.guild.voice_client
+    if not client.is_paused():
+        client.pause()
+
+@bot.command()
+async def play(ctx, url):
+    print("play")
+    client = ctx.guild.voice_client
+
+    if client and client.channel:
+        video = Video(url)
+        musics[ctx.guild].append(video)
+    else:
+        channel = ctx.author.voice.channel
+        video = Video(url)
+        musics[ctx.guild]=[]
+        client = await channel.connect()
+        await ctx.send(f"Je lance : {video.url}")
+        play_song(client, musics[ctx.guild], video)
+
 
 
 
